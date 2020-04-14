49dd271ad7ef7b7de7a52fee9af0d20f5f7888f0
jehough <teachlife@outlook.com>
2020-04-14 19:04:23 -0400

    restarting to do different setup

--- app/models/budget.rb
+++
@@ -1,4 +0,0 @@
-class Budget < ApplicationRecord
-  belongs_to :user
-  has_many :categories
-end
--- app/models/category.rb
+++
@@ -1,5 +0,0 @@
-class Category < ApplicationRecord
-  belongs_to :budget
-  has_many :transactions
-
-end
