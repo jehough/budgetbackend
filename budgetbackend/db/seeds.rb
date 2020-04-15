8d1bea5c8a2cc40abd41e411266884aa0c5fb848
jehough <teachlife@outlook.com>
2020-04-14 20:54:22 -0400

    adds seed data

--- budgetbackend/db/seeds.rb
+++ budgetbackend/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+jane = User.create(email: "prettyjane@email.com", password: "secret")
+joe = User.create(email: "handsomejoe@email.com", password: "secret")
+Budget.create(title: "Jane's Budget", total: 3000, user: jane)
+Budget.create(title: "Joe's Budget", total: 2500, user: joe)
+Budget.create(title: "Little Joe's Budget", total: 25.50, user: joe)
